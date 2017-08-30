0edeb3a992244b495b2a10aedf967f41e36a7ffe
poc2-unimelb <31395015+poc2-unimelb@users.noreply.github.com>
2017-08-30 15:07:06 +1000

    Add annotation to each word

--- sample/js/reading.js
+++ sample/js/reading.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 
     $('p').css('font-size','30px');
-    
+    $("div[class='annotag open']").hide();
+
     $('p').each(function(){
             var text = $(this).html().split(' '),
                 len = text.length,
@@ -31,8 +32,42 @@ $(document).ready(function(){
             function() {
                 var utterance = new SpeechSynthesisUtterance(this.$element.text());
                 speechSynthesis.speak(utterance);
-            },20
+            },30
           );
+
+          $( this ).eyeIn(
+            function() {
+
+                var border = this.border;
+                var text = this.$element.text();
+
+                $.ajax({
+                     url:"https://glosbe.com/gapi/translate?from=eng&dest=zh&format=json&phrase="+text+"&pretty=true",
+                     dataType: 'jsonp', // Notice! JSONP <-- P (lowercase)
+                     success:function(json){
+                         if(json.result=='ok' && json.tuc && json.tuc[0] && json.tuc[0].phrase && json.tuc[0].meanings){
+                            $(".word").text(text);
+                            $(".wordmeaning").text(json.tuc[0].meanings[0].text)
+                            $(".wordtranslate").text(json.tuc[0].phrase.text);
+                            $("div[class='annotag open']").css("left",border[1]+10);
+                            $("div[class='annotag open']").css("top",border[3]);
+                            $("div[class='annotag open']").show();
+
+                            setInterval(function(){
+                              $("div[class='annotag open']").hide();
+                            },3000);
+
+                         }
+                         else
+                          console.log("translate error");
+                     },
+                     error:function(){
+                         console.log("glosbe error");
+                     }      
+                });
+            },30
+          );
+
     });
 
   	$.eyeSuspend(
